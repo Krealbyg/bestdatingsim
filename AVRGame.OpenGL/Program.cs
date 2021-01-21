6ec8430f1cfe9cbf3da148c1faf6c1e7fc084cbf
avr_informatica <35996754+R-Heemskerk@users.noreply.github.com>
2021-01-21 17:06:30 +0100

    Initial commit

---
+++ AVRGame.OpenGL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AVRGame.NetStandardLibrary;
+
+namespace AVRGame.OpenGL
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new AVRGame.NetStandardLibrary.Game())
+                game.Run();
+        }
+    }
+}
