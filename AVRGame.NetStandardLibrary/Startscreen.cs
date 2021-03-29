35e74910849a1a4d5bc52e7745d842f9f3a0cbe1
LiamTemming <liamktemming@gmail.com>
2021-03-29 10:43:32 +0200

    added level select screen on startscreen for easier testing. also slight start on timeskip 1

--- AVRGame.NetStandardLibrary/Startscreen.cs
+++ AVRGame.NetStandardLibrary/Startscreen.cs
@@ -17,8 +17,11 @@ namespace AVRGame.NetStandardLibrary
     {
         private FieryTale fieryTale;
 
+        private bool levelselect;
+
         //lists
         private List<Button> buttons;
+        private List<Button> levels;
 
         //background
         private Texture2D background;
@@ -38,6 +41,7 @@ namespace AVRGame.NetStandardLibrary
             //textures
             background = fieryTale.Content.Load<Texture2D>("MainMenu");
 
+            #region buttons
             //loads buttons
             var startButton = new Button(fieryTale.Content.Load<Texture2D>("WhiteRectangle"), fieryTale.Content.Load<SpriteFont>("Names"), fieryTale)
             {
@@ -45,28 +49,154 @@ namespace AVRGame.NetStandardLibrary
                 ButtonText = "Start Game",
             };
 
-            var testButton = new Button(fieryTale.Content.Load<Texture2D>("WhiteRectangle"), fieryTale.Content.Load<SpriteFont>("Names"), fieryTale)
+            var selectButton = new Button(fieryTale.Content.Load<Texture2D>("WhiteRectangle"), fieryTale.Content.Load<SpriteFont>("Names"), fieryTale)
             {
                 ButtonPosition = new Vector2(0, 380),//360(center of screen) + 20
+                ButtonText = "Level select",
+            };
+
+            //levels
+            var day1Button = new Button(fieryTale.Content.Load<Texture2D>("WhiteRectangle"), fieryTale.Content.Load<SpriteFont>("Names"), fieryTale)
+            {
+                ButtonPosition = new Vector2(0, 20),
+                ButtonText = "Day 1",
+            };
+            var day2Button = new Button(fieryTale.Content.Load<Texture2D>("WhiteRectangle"), fieryTale.Content.Load<SpriteFont>("Names"), fieryTale)
+            {
+                ButtonPosition = new Vector2(0, 122),
+                ButtonText = "Day 2",
+            };
+            var day5Button = new Button(fieryTale.Content.Load<Texture2D>("WhiteRectangle"), fieryTale.Content.Load<SpriteFont>("Names"), fieryTale)
+            {
+                ButtonPosition = new Vector2(0, 224),
+                ButtonText = "Day 5",
+            };
+            var day6Button = new Button(fieryTale.Content.Load<Texture2D>("WhiteRectangle"), fieryTale.Content.Load<SpriteFont>("Names"), fieryTale)
+            {
+                ButtonPosition = new Vector2(0, 326),
+                ButtonText = "Day 6",
+            };
+            var day10Button = new Button(fieryTale.Content.Load<Texture2D>("WhiteRectangle"), fieryTale.Content.Load<SpriteFont>("Names"), fieryTale)
+            {
+                ButtonPosition = new Vector2(0, 428),
+                ButtonText = "Day 10",
+            };
+            var endingButton = new Button(fieryTale.Content.Load<Texture2D>("WhiteRectangle"), fieryTale.Content.Load<SpriteFont>("Names"), fieryTale)
+            {
+                ButtonPosition = new Vector2(0, 530),
+                ButtonText = "Ending",
+            };
+            var testButton = new Button(fieryTale.Content.Load<Texture2D>("WhiteRectangle"), fieryTale.Content.Load<SpriteFont>("Names"), fieryTale)
+            {
+                ButtonPosition = new Vector2(0, 632),
                 ButtonText = "Test level",
             };
 
             //creates button click events
             startButton.Click += StartButton_Click;
             testButton.Click += TestButton_Click;
+            selectButton.Click += SelectButton_Click;
+            day1Button.Click += Day1Button_Click;
+            day2Button.Click += Day2Button_Click;
+            day5Button.Click += Day5Button_Click;
+            day6Button.Click += Day6Button_Click;
+            day10Button.Click += Day10Button_Click;
+            endingButton.Click += EndingButton_Click;
 
             //puts buttons in list
             buttons = new List<Button>()
             {
                 startButton,
-                testButton
+                selectButton
             };
+
+            levels = new List<Button>
+            {
+                day1Button,
+                day2Button,
+                day5Button,
+                day6Button,
+                day10Button,
+                endingButton,
+                testButton,
+            };
+            #endregion
+
             base.LoadContent();
         }
 
-        private void TestButton_Click(object sender, EventArgs e)//test level button event
+        #region click events
+        private void EndingButton_Click(object sender, EventArgs e)
         {
-            if (fieryTale.currentLevel == 0)//only when in no level
+            if (fieryTale.currentLevel == 0 && levelselect == true)//day choice button, only on select
+            {
+                fieryTale.currentLevel = 9;
+                fieryTale.choiceMoment = false;//gives back control
+                MediaPlayer.Stop();//stops menu song
+            }
+        }
+
+        private void Day10Button_Click(object sender, EventArgs e)
+        {
+            if (fieryTale.currentLevel == 0 && levelselect == true)//day choice button, only on select
+            {
+                fieryTale.currentLevel = 7;
+                fieryTale.choiceMoment = false;//gives back control
+                MediaPlayer.Stop();//stops menu song
+            }
+        }
+
+        private void Day6Button_Click(object sender, EventArgs e)
+        {
+            if (fieryTale.currentLevel == 0 && levelselect == true)//day choice button, only on select
+            {
+                fieryTale.currentLevel = 6;
+                fieryTale.choiceMoment = false;//gives back control
+                MediaPlayer.Stop();//stops menu song
+            }
+        }
+
+        private void Day5Button_Click(object sender, EventArgs e)
+        {
+            if (fieryTale.currentLevel == 0 && levelselect == true)//day choice button, only on select
+            {
+                fieryTale.currentLevel = 4;
+                fieryTale.choiceMoment = false;//gives back control
+                MediaPlayer.Stop();//stops menu song
+            }
+        }
+
+        private void Day2Button_Click(object sender, EventArgs e)
+        {
+            if (fieryTale.currentLevel == 0 && levelselect == true)//day choice button, only on select
+            {
+                fieryTale.currentLevel = 3;
+                fieryTale.choiceMoment = false;//gives back control
+                MediaPlayer.Stop();//stops menu song
+            }
+        }
+
+        private void Day1Button_Click(object sender, EventArgs e)
+        {
+            if (fieryTale.currentLevel == 0 && levelselect == true)//day choice button, only on select
+            {
+                fieryTale.currentLevel = 2;
+                fieryTale.choiceMoment = false;//gives back control
+                MediaPlayer.Stop();//stops menu song
+            }
+        }
+
+        private void SelectButton_Click(object sender, EventArgs e)
+        {
+            if (fieryTale.currentLevel == 0 && levelselect == false)//only when in no level and not on select
+            {
+                levelselect = true;
+            }
+        }
+
+        private void TestButton_Click(object sender, EventArgs e)//test level button event only on select
+        {
+            if (fieryTale.currentLevel == 0 && levelselect == true)//only when in no level
             {
                 fieryTale.currentLevel = 100;//number we will never get to normally
                 fieryTale.choiceMoment = false;//gives back control
@@ -76,17 +206,20 @@ namespace AVRGame.NetStandardLibrary
 
         private void StartButton_Click(object sender, EventArgs e)//start game button event
         {
-            if (fieryTale.currentLevel == 0)//only when in no level
+            if (fieryTale.currentLevel == 0 && levelselect == false)//only when in no level and not on select
             {
                 fieryTale.currentLevel = 1;//starts first level
                 fieryTale.choiceMoment = false;//gives back control
                 MediaPlayer.Stop();//stops menu song
             }
         }
+        #endregion
 
         public override void Update(GameTime gameTime)
         {   
             foreach (var button in buttons)//updates all the buttons
+                button.Update(gameTime);
+            foreach (var button in levels)//updates all the buttons
                 button.Update(gameTime);
 
             base.Update(gameTime);
@@ -98,10 +231,18 @@ namespace AVRGame.NetStandardLibrary
 
             if (fieryTale.currentLevel == 0)
             {
-                fieryTale.spriteBatch.Draw(background, new Rectangle(0, 0, 1280, 720), Color.White);
-                foreach (var button in buttons)//draws buttons
-                    button.Draw(gameTime);
                 fieryTale.choiceMoment = true;//takes away control
+                fieryTale.spriteBatch.Draw(background, new Rectangle(0, 0, 1280, 720), Color.White);
+                if (levelselect == false)
+                {
+                    foreach (var button in buttons)//draws buttons
+                        button.Draw(gameTime);
+                }
+                if (levelselect == true)
+                {
+                    foreach (var button in levels)//Draws
+                        button.Draw(gameTime);
+                }
             }
 
             fieryTale.spriteBatch.End();
---
+++ AVRGame.NetStandardLibrary/TimeSkip1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Content;
+using GameMechanics;
+
+namespace AVRGame.NetStandardLibrary
+{
+    public class TimeSkip1 : DrawableGameComponent
+    {
+        FieryTale fieryTale;
+
+        public TimeSkip1(FieryTale fieryTale) : base(fieryTale)
+        {
+            this.fieryTale = fieryTale;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (fieryTale.currentLevel == 4 && fieryTale.attackedSomeone == false)
+            {
+                
+                
+                base.Update(gameTime);
+            }
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            if (fieryTale.currentLevel == 4 && fieryTale.attackedSomeone == false)
+            {
+                fieryTale.spriteBatch.Begin();
+
+
+
+                fieryTale.spriteBatch.End();
+            }
+        }
+    }
+}
